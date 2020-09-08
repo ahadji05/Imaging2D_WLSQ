98df7e2031d8b57a8b4137bcfec921d1b3ce2835
ahadji05 <andreash95.AH@gmail.com>
2020-09-08 16:08:07 +0300

    run demo

--- run_demo.sh
+++ run_demo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python main.py demo-data/velmod.csv demo-data/problemSetup.txt demo-data/seismicShots/ demo-result
+python main.py demo-data/velmod.csv demo-data/problemSetup.txt demo-data/seismicShots/ demo-result host
 
