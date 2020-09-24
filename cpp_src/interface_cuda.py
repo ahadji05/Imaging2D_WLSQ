b7d16dfee751015af31b7027a50395044d78f573
ahadji05 <andreash95.AH@gmail.com>
2020-09-24 12:03:34 +0300

    script to allow interface between Python and cuda-compiled code

--- cpp_src/interface_cuda.py
+++ cpp_src/interface_cuda.py
@@ -1,16 +1,15 @@
 
 import sys
 import os 
-from ctypes import CDLL, c_int, c_void_p, c_float
+from ctypes import CDLL, c_int, c_void_p
 from numpy.ctypeslib import ndpointer
 import numpy as np
 
 interface_path = os.path.dirname(__file__)
 
 extrapolation_gpu = CDLL(interface_path + '/extrapolation_gpu.so')
-extrapolation_sh_gpu = CDLL(interface_path + '/extrapolation_sh_gpu.so')
-extrapolation_sh_AoS_gpu = CDLL(interface_path + '/extrapolation_sh_AoS_gpu.so')
 extrapolation_revOp_gpu = CDLL(interface_path + '/extrapolation_revOp_gpu.so')
+extrapolation_sh_revOp_gpu = CDLL(interface_path + '/extrapolation_sh_revOp_gpu.so')
 
 
 #----------------------------------------------------------------------
@@ -28,25 +27,6 @@ extrapolate.argtypes = [c_int, c_int, c_int,
 
 #----------------------------------------------------------------------
 #
-extrapolate_sh = extrapolation_sh_gpu.extrapolate
-extrapolate_sh.restype = c_void_p
-extrapolate_sh.argtypes = [c_int, c_int, c_int,
-                   c_int, c_int, c_int, c_int,
-                   ndpointer( dtype=np.complex64, flags=("C","A") ),
-                   ndpointer( dtype=np.complex64, flags=("C","A") )]
-
-#----------------------------------------------------------------------
-#
-extrapolate_sh_AoS = extrapolation_sh_AoS_gpu.extrapolate
-extrapolate_sh_AoS.restype = c_void_p
-extrapolate_sh_AoS.argtypes = [c_int, c_int, c_int,
-                   c_int, c_int, c_int, c_int,
-                   ndpointer( dtype=np.complex64, flags=("C","A") ),
-                   ndpointer( dtype=np.complex64, flags=("C","A") )]
-
-
-#----------------------------------------------------------------------
-#
 extrapolate_revOp = extrapolation_revOp_gpu.extrapolate
 extrapolate_revOp.restype = c_void_p
 extrapolate_revOp.argtypes = [c_int, c_int, c_int,
@@ -57,4 +37,13 @@ extrapolate_revOp.argtypes = [c_int, c_int, c_int,
                    ndpointer( dtype=np.complex64, flags=("C","A") ),
                    ndpointer( dtype=np.float32, flags=("C","A") )]
 
-                   
+
+
+#----------------------------------------------------------------------
+#
+extrapolation_sh_revOp = extrapolation_sh_revOp_gpu.extrapolate
+extrapolation_sh_revOp.restype = c_void_p
+extrapolation_sh_revOp.argtypes = [c_int, c_int, c_int,
+                   c_int, c_int, c_int, c_int,
+                   ndpointer( dtype=np.complex64, flags=("C","A") ),
+                   ndpointer( dtype=np.complex64, flags=("C","A") )]
